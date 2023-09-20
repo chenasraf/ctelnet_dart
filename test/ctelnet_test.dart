36cb91ccea45eb7f3890139f582ec2076f585bdf
Chen Asraf <casraf@pm.me>
2023-09-21 01:34:37 +0300

    initial commit

---
+++ test/ctelnet_test.dart
@@ -0,0 +1,16 @@
+import 'package:ctelnet/ctelnet.dart';
+import 'package:test/test.dart';
+
+void main() {
+  group('A group of tests', () {
+    final awesome = Awesome();
+
+    setUp(() {
+      // Additional setup goes here.
+    });
+
+    test('First Test', () {
+      expect(awesome.isAwesome, isTrue);
+    });
+  });
+}
